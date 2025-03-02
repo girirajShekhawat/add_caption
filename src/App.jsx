f64e58340a32b3bfcb95a8537ba4181859d12052
girirajSpurFit <giriraj@spur.fit>
2025-03-02 16:35:35 +0530

    wip:project setup

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+ 
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+   <div>
+     <h1 className='text-3xl font-bold underline'>Add Captions</h1>
+   </div>
+  )
+}
+
+export default App
